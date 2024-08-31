496f7ea2bc156cf2645871ab64df4fc7ccea0d73
VANDRANKI <72809214+VANDRANKI@users.noreply.github.com>
2024-08-31 14:34:18 -0400

    changed the height and width of the icons

--- src/components/Icon/index.jsx
+++ src/components/Icon/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { icons } from './data'
-const Icon = ({ icon, width = 6, height = 6, onClick = null }) => {
+const Icon = ({ icon, width = 4, height = 4, onClick = null }) => {
     return (
-        <div onClick={onClick} className={`w-${width}  h-${height} items-center justify-center cursor-pointer`}><img style={{ width: '100%', height: '100%' }} src={icons[icon]} alt="img not found" fit="contain" /></div>
+        <div onClick={onClick} className={`w-6  h-6 items-center justify-center cursor-pointer`}><img style={{ width: '100%', height: '100%' }} src={icons[icon]} alt="img not found" fit="contain" /></div>
     )
 }
 
